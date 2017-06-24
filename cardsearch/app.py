df883c85eabe90ceb972b3010b454de6e5beb555
glenn mohre <glenn.mohre@gmail.com>
2017-06-24 12:34:39 -0400

    second commity

--- cardsearch/app.py
+++ cardsearch/app.py
@@ -3,71 +3,51 @@ import urllib2
 import base64
 import boto3
 
-from botocore.exceptions import ClientError
-
 from chalice import Chalice
 from chalice import NotFoundError
 
 app = Chalice(app_name='cardsearch')
-app.debug = True
 
+REGION = 'us-east-2'
 SCRYFALL_API_URL = 'https://api.scryfall.com/cards'
-S3 = boto3.client('s3', region_name='us-east-2')
-S3_RESOURCE = boto3.resource('s3', region_name='us-east-2')
-BUCKET_KEY = 'custom-cards'
-CARD_KEY = 'my-card'
 
-#def _init_bucket():
-#    try:
-#	S3.meta.client.head_bucket(Bucket='mybucket')
-#    except ClientError as e:
-#        error_code = int(e.response['Error']['Code'])
-#	if error_code == 404:
-#            exists = False
-#    return bucket, exists
+S3 = boto3.client('s3')
+
+BUCKET_KEY =  'custom-cards'
+CARD_KEY = 'my-card.json'
+CARD_IMAGE = '{}.png'.format(CARD_KEY)
 
 
-@app.route('/cards', methods=['GET'])
+@app.route('/card', methods=['GET'])
 def card_index():
-#    bucket, exists = _init_bucket()
-#    bucket_keys_json = {[k.key for k in bucket.objects.all()]}
-#    if exists:
-#	return json.load(bucket_keys_json)
-    return "Nope"
+    return "OK"
 
-@app.route('/cards/text/{query}', methods=['PUT'])
-def card_put(query):
-    request = app.current_request
-    output = S3.put_object(Bucket=BUCKET_KEY, Key=CARD_KEY,
-	Body=json.dumps(request.json_body))
-    return "No Luck"
-
-@app.route('/card/{key}', methods=['GET'])
-def card_get(key):
-    try:
-	response = S3.get_object(Bucket=BUCKET_KEY, Key=key)
-        return json.loads(response['Body'].read())
-    except ClientError as e:
-        raise NotFoundError(key)
-    return "No Luck"
-
-@app.route('/card/{key}', methods=['POST'])#, content_types=['application/octet-stream'])
+@app.route('/card/{key}', methods=['POST'])
 def card_post(key):
+    """
+	Input: 
+		raw payload of base64 encoded image
+		key to query M:tG Database
+	Output:
+		Location of image
+		Location of card JSON
+    """
     body = app.current_request.raw_body
     image = base64.b64decode(body)
 
     filename = '{}.png'.format(CARD_KEY)
-    card_url = '{}/search?q={}'.format(SCRYFALL_API_URL, key)
+    card_query_url = '{}/search?q={}'.format(SCRYFALL_API_URL, key)
 
-    card_query_resp = urllib2.urlopen(card_url)
+    card_query_resp = urllib2.urlopen(card_query_url)
     card_query_resp = card_query_resp.read()
 
     card_query_json = json.loads(card_query_resp)
     cards = card_query_json['data']
     card = cards.pop()
 
-    card_data = {'name':card.get('name'), 'oracle_text':card.get('oracle_text')} if card else {'name':key, 'oracle_text':key}
-   
+    card_data = {
+	'name':card.get('name'),
+	'oracle_text':card.get('oracle_text')}
 
     image_s3 = S3.put_object(
 	Bucket=BUCKET_KEY,
@@ -81,6 +61,6 @@ def card_post(key):
 	ACL='public-read',
 	Key=CARD_KEY,
 	Body=json.dumps(card_data))
-    return image_s3
 
-
+    return {'image':'https://s3.{}.amazonaws.com/{}/{}'.format(REGION, BUCKET_KEY, filename),
+	'card':'https://s3.{}.amazonaws.com/{}/{}'.format(REGION, BUCKET_KEY, CARD_KEY)}
